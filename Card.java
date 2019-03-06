4cbdaac32867f0b21fbb40b9aecbb1495b1ce0a4
stephenfatuzzo <48285841+stephenfatuzzo@users.noreply.github.com>
2019-03-06 08:51:02 -0500

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,41 +1,37 @@
-/**
- * Card.java
- *
- * <code>Card</code> represents a playing card.
- */
 public class Card {
-
-	/**
-	 * String value that holds the suit of the card
-	 */
+	
 	private String suit;
-
-	/**
-	 * String value that holds the rank of the card
-	 */
 	private String rank;
-
-	/**
-	 * int value that holds the point value.
-	 */
 	private int pointValue;
-
-
-   /**
-	 * Creates a new <code>Card</code> instance.
-	 *
-	 * @param cardRank  a <code>String</code> value
-	 *                  containing the rank of the card
-	 * @param cardSuit  a <code>String</code> value
-	 *                  containing the suit of the card
-	 * @param cardPointValue an <code>int</code> value
-	 *                  containing the point value of the card
-	 */
+	
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
 		rank = cardRank;
 		suit = cardSuit;
 		pointValue = cardPointValue;
-		//Could have cardRank be rank, then you say this.rank = rank;
-		//Can print rank outside of class using System.out.println(d2.rank);
-		//Why not just use the d2.rank instead of making new methods?
 	}
+	
+	public String suit() {
+		return suit;
+  	}
+	
+	public String rank() {
+		return rank;
+	}
+	
+	public int pointValue() {
+		return pointValue;
+	}
+	
+	public boolean matches(Card otherCard) {
+		if ((otherCard.rank().equals(rank)) && (otherCard.suit().equals(suit)) && (otherCard.pointValue() == pointValue)) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return (rank + " of " + suit + " (point value = " + pointValue + ")");
+	}
+}
+
