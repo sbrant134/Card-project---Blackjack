11c2b5d7470825c84f5f6167b7079e816af1fc7b
sbrant134 <48285888+sbrant134@users.noreply.github.com>
2019-03-06 09:03:29 -0500

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -1,1 +1,26 @@
-public class Blackjack
+public class Blackjack {
+  //make "stay" into a boolean
+  //create values for points of each player
+  
+  public void playGame() {
+    
+  }
+  
+  public boolean gameOver() {
+    if ( (points1 > 21) || (points2 > 21) ) {
+      return true;
+    }
+    else if ( (stay1==true) && (stay2==true) ) {
+      return true;
+    }
+    else if ( (pInitial1==21) || (pInitial2==21) ) {
+      return true;
+    }
+    return false;
+    
+  }
+  
+  public void whoWon() {
+    
+  }
+}
