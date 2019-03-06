9509fe478a9d9507f900e5089d8ba417081c51fe
stephenfatuzzo <48285841+stephenfatuzzo@users.noreply.github.com>
2019-03-06 08:45:53 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,41 @@
+/**
+ * Card.java
+ *
+ * <code>Card</code> represents a playing card.
+ */
+public class Card {
+
+	/**
+	 * String value that holds the suit of the card
+	 */
+	private String suit;
+
+	/**
+	 * String value that holds the rank of the card
+	 */
+	private String rank;
+
+	/**
+	 * int value that holds the point value.
+	 */
+	private int pointValue;
+
+
+   /**
+	 * Creates a new <code>Card</code> instance.
+	 *
+	 * @param cardRank  a <code>String</code> value
+	 *                  containing the rank of the card
+	 * @param cardSuit  a <code>String</code> value
+	 *                  containing the suit of the card
+	 * @param cardPointValue an <code>int</code> value
+	 *                  containing the point value of the card
+	 */
+	public Card(String cardRank, String cardSuit, int cardPointValue) {
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
+		//Could have cardRank be rank, then you say this.rank = rank;
+		//Can print rank outside of class using System.out.println(d2.rank);
+		//Why not just use the d2.rank instead of making new methods?
+	}
