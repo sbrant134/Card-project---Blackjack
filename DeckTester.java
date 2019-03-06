3fb9ef4c7e042b20a044fd037cf3285f58ef7831
stephenfatuzzo <48285841+stephenfatuzzo@users.noreply.github.com>
2019-03-06 09:01:23 -0500

    Update DeckTester.java

--- DeckTester.java
+++ DeckTester.java
@@ -1,36 +1,13 @@
-/**
- * This is a class that tests the Deck class.
- */
-public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	private static String[] ranks;
-	private static String[] suits;
-	private static int[] values;
+public class DeckTester {
 	
 	public static void main(String[] args) {
-		ranks = new String[2];
-		ranks[0] = "1";
-		ranks[1] = "2";
-		suits = new String[4];
-		suits[0] = "D";
-		suits[1] = "H";
-		suits[2] = "C";
-		suits[3] = "S";
-		values = new int[2];
-		values[0] = 1;
-		values[1] = 2;
-
-//Above is the tester "deck" used to create the deck.
+		private static String[] ranks = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		private static String[] suits = {"Clubs", "Hearts", "Spades", "Diamonds"};
+		private static int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
 		Deck d1 = new Deck(ranks, suits, values);
-		System.out.println(d1.isEmpty());
-		System.out.println(d1.size());
-		System.out.println(d1.deal());
 	}
 }
-//Above is the implementation of a deck and a call to its methods.
+
 
