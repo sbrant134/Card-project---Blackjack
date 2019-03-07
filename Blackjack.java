36b90298cb26866ae936667cdfd315e210a2928e
sbrant134 <48285888+sbrant134@users.noreply.github.com>
2019-03-07 09:45:09 -0500

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -3,7 +3,10 @@ public class Blackjack {
   //create values for points of each player
   
   public void playGame() {
-    
+    d1.deal();
+    d1.deal();
+    while (!gameOver) {
+      
   }
   
   public boolean gameOver() {
@@ -21,6 +24,21 @@ public class Blackjack {
   }
   
   public void whoWon() {
-    
+    if (points1<=21 && points2<=21) {
+      if (points1>points2) {
+        System.out.println("Player 1 wins");
+      }
+      else if (points2>points1) {
+        System.out.println("Player 2 wins");
+      }
+      System.out.println("tie");
+    }
+    else if (points1>21 && points2>21) {
+      System.out.println("both bust");
+    }
+    else if (points1>21 && points2<=21) {
+      System.out.println("Player 2 wins");
+    }
+    System.out.println("Player 1 wins");
   }
 }
